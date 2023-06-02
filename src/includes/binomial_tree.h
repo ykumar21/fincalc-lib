a567d6ca52dd364c7719730a8d1e13209fa153cb
Yash <yash20008@gmail.com>
2023-06-02 13:06:28 +0530

    Fixed container templating and added documentation

--- src/includes/binomial_tree.h
+++ src/includes/binomial_tree.h
@@ -3,9 +3,17 @@
 
 #include "tree.h"
 #include <math.h>
+#include <stdint.h>  /* types */
+
+#define _PRICE_TREE_DEST "price.txt"
+#define _OPTION_TREE_DEST "value.txt"
 
 namespace BinomialTree {
 
+/**
+ * @brief Market parameters for model 
+ * 
+ */
 struct Config {
   double up_factor;
   double down_factor;
@@ -13,24 +21,31 @@ struct Config {
   double risk_neutral_probability;
   double risk_free_interest;
   double initial_price;
-  int steps;
+  int    steps;
   double strike_price;
 }; // namespace Config
 
+
+/**
+ * @brief Default configuration for the binomial tree
+ * 
+ */
 struct Config DEFAULT_CONFIG {
   .up_factor = 1.2, .down_factor = 0.8, .market_probability = 0.6,
   .risk_free_interest = 0.02, .initial_price = 100, .steps = 4,
   .strike_price = 110
 };
 
+/**
+ * @brief Binomial Tree model to price options 
+ * 
+ */
 class Model {
 private:
-  Tree<double> *stock_price_tree;
-  Tree<double> *option_value_tree;
-
-  Config parameters;
-
-  float risk_neutral_probability = 0;
+  Tree<double> *stock_price_tree;  /* future price movement of stock */
+  Tree<double> *option_value_tree; /* future option value */
+  Config       parameters; /* market parameters */
+  float        risk_neutral_probability = 0;
 
   void calculuate_risk_neutal_probability() {
     risk_neutral_probability =
@@ -41,9 +56,12 @@ private:
   void build_price_tree() {
     // add the initial price of the stock
     (*stock_price_tree).add(parameters.initial_price);
-    for (int i = 1; i < (int)pow(2, parameters.steps) - 1; ++i) {
+
+    for (int i = 1; i < (int)pow(2, parameters.steps); ++i) {
+      std::cout << *stock_price_tree << std::endl;
       // get parent element
       double previous_price = (*stock_price_tree).get_parent(i);
+
       // compute the current price
       double price_multipler = parameters.down_factor * (i % 2 != 0) +
                                parameters.up_factor * (i % 2 == 0);
@@ -69,6 +87,7 @@ private:
           std::exp(-parameters.risk_free_interest) *
           (risk_neutral_probability * next_values.second +
            (1 - risk_neutral_probability) * next_values.first);
+
       (*option_value_tree)
           .set(i, std::max(early_excercise_value, expectation_value));
     }
@@ -77,20 +96,32 @@ private:
 public:
   Model(Config parameters) {
     this->parameters = parameters;
-    this->stock_price_tree = new Tree<double>();
-    this->option_value_tree = new Tree<double>();
+    this->stock_price_tree =
+        new Tree<double>((int)std::pow(2, this->parameters.steps) - 1);
+    this->option_value_tree =
+        new Tree<double>((int)std::pow(2, this->parameters.steps) - 1);
   };
 
-  void _exec() {
+  void run() {
     // Compute risk neutral prob
     calculuate_risk_neutal_probability();
     // Build the price tree
+    printf("Building Price Tree... \n");
     this->build_price_tree();
-    this->build_option_tree();
-    printf("-- Current Value Of Call Option = %f\n", (*option_value_tree).get(0));
-  }
-};
 
+    printf("Building Option Value Tree... \n");
+    this->build_option_tree();
+    printf("-- Current Value Of Call Option = %f\n",
+           (*option_value_tree).get(0));
+  }
+
+  /**
+   * @brief Save model outputs to text file 
+   * 
+   * @return std::string Output ID  
+   */
+  std::string save() {}
+};
 } // namespace BinomialTree
 
 #endif
--- src/includes/tree.h
+++ src/includes/tree.h
@@ -5,23 +5,45 @@
 #include <cstdlib> /* malloc */
 #include <iostream>
 #include <stdexcept> /* exceptions */
+#include <stdint.h>  /* types */
 
 #define MAX_SIZE 15
 
+/**
+ * @brief Class For Modeling Binary Tree
+ * 
+ * @tparam T Data Type of tree nodes
+ */
 template <typename T> class Tree {
 private:
-  T *container;
-  int max_size = MAX_SIZE;
-  int cur_index = 0;
+  const static int _MAX_SIZE = MAX_SIZE;
+
+  T            *container = nullptr;
+  uint16_t     size = _MAX_SIZE;
+  uint8_t      levels;
+  uint16_t     cur_index = 0;
 
 public:
-  Tree<T>(int size = 2048) { container = (T *)malloc(sizeof(int) * size); };
+  Tree<T>(int size = Tree::_MAX_SIZE) {
+    this->size = size; 
+    container = (T *)malloc(sizeof(T) * this->size);
+    for (auto i = 0; i < this->size; ++i) {
+      container[i] = 0.0;
+    }
+  };
 
+  /**
+   * @brief Overloaded << operator to print tree to output stream
+   * 
+   * @param os Output stream (destination)
+   * @param t  Tree Object 
+   * @return std::ostream& 
+   */
   friend std::ostream &operator<<(std::ostream &os, Tree &t) {
     os << t.get(0) << '\n';
-    int next_break = 2;
-    int row_count = 0;
-    for (int i = 1; i < t.max_size; ++i) {
+    uint16_t  next_break = 2; /* next break line */ 
+    uint8_t   row_count = 0;  /* current number of items in row */ 
+    for (int i = 1; i < t.size; ++i) {
       os << t.get(i) << ' ';
       row_count++;
       if (row_count == next_break) {
@@ -33,29 +55,54 @@ public:
     return os;
   };
 
+  /**
+   * @brief Add item to the tree
+   * 
+   * @param item Data node item
+   */
   void add(T item) {
-    if (cur_index == max_size) {
+    if (cur_index > this->size) {
       throw std::invalid_argument("Tree is full!");
     }
     container[cur_index++] = item;
     return;
   };
 
-  T get(int index) {
-    if (index > max_size) {
+  /**
+   * @brief Get item at index
+   * 
+   * @param index node index
+   * @return T data item at node 
+   */
+  T get(uint16_t index) {
+    if (index > this->size) {
       throw std::invalid_argument("Index out of bounds.");
     }
     return container[index];
   }
-  void set(int index, T item) {
-    if (index > max_size) {
+  
+  /**
+   * @brief Set node item at index
+   * 
+   * @param index Node index
+   * @param item Data item
+   */
+  void set(uint16_t index, T item) {
+    if (index > this->size) {
+      printf("%d - %d\n", index, size);
       throw std::invalid_argument("[Tree-Set] Index out of bound");
     }
     container[index] = item;
   }
 
-  T get_parent(int index) {
-    if (index > max_size) {
+  /**
+   * @brief Get the parent object
+   * 
+   * @param index node index
+   * @return T Parent node data
+   */
+  T get_parent(uint16_t index) {
+    if (index > this->size) {
       throw std::invalid_argument("Index out of bounds!");
     }
 
@@ -66,8 +113,14 @@ public:
     }
   }
 
-  std::pair<T, T> get_children(int index) {
-    if (floor(std::log2(index)) == floor(std::log2(max_size)) + 1) {
+  /**
+   * @brief Get the children object
+   * 
+   * @param index parent index
+   * @return std::pair<T, T> children node data
+   */
+  std::pair<T, T> get_children(uint16_t index) {
+    if (floor(std::log2(index)) == floor(std::log2(this->size)) + 1) {
       throw std::invalid_argument("No children for leaf node");
     }
     std::pair<T, T> children;
